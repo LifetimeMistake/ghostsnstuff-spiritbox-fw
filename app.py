3b1ddd16bae389fbb6a02c4cc06421e0fc4f061f
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-10-25 14:09:55 +0000

    Add initial main loop

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 from ghostsnstuff_spiritbox_fw.agents import Writer
+from ghostsnstuff_spiritbox_fw.scenario import ScenarioDefinition, load_scenario
+from ghostsnstuff_spiritbox_fw.events import EventTimeline
+from ghostsnstuff_spiritbox_fw.game import GameRuntime, RuntimeConfig
+from ghostsnstuff_spiritbox_fw.hal.emf import EMFDriver
+import ghostsnstuff_spiritbox_fw.logging as logging
+import json
 
 load_dotenv()
 
 client = OpenAI()
+config = RuntimeConfig()
+timeline = EventTimeline()
+logging.set_timeline(timeline)
 
-writer = Writer(client, "gpt-4o-mini", 0.5)
-response = writer.generate(
-    "A group of ghost hunters is currently in an attic. The goal of the scenario is to restore the lost spirit's memories and allow it to escape before the evil spirit consumes it.",
-    "Lost spirit and evil secondary spirit",
-    None
+###### Configuration begins here
+scenario_file = "scenarios/scenario_2.json"
+config.activity_grow_factor = 0.1
+###### Configuration ends here
+
+###### Initialize runtime
+writer = Writer(
+    client=client,
+    model=config.curator_model,
+    temperature=config.curator_temperature
+)
+base_scenario = load_scenario(scenario_file)
+runtime = GameRuntime(
+    client=client,
+    scenario=base_scenario,
+    config=config,
+    timeline=timeline
 )
 
-print(f"Producer notes: {response.reasoning}")
-scenario = response.scenario
-print(scenario.model_dump_json(indent=2))
+###### Initialize HAL
+emf_driver = EMFDriver()
+
+logging.info("Init OK")
+logging.info("Starting scenario")
+
+###### Run main loop
+runtime.game_state.reset()
+while True:
+    user_query = "..."
+    turn_result = runtime.execute(user_query)
+    # Act...
