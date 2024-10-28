50cec4b36ed210220f00066954e729e27a1e5d28
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2024-10-28 01:26:27 +0100

    Rewrite main to use new drivers

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from dotenv import load_dotenv
 from ghostsnstuff_spiritbox_fw.agents import Writer
 from ghostsnstuff_spiritbox_fw.scenario import ScenarioDefinition, load_scenario
 from ghostsnstuff_spiritbox_fw.events import EventTimeline
-from ghostsnstuff_spiritbox_fw.game import GameRuntime, RuntimeConfig
+from ghostsnstuff_spiritbox_fw.game import GameRuntime, RuntimeConfig, GhostActions, RuntimeExecutionResult
 import ghostsnstuff_spiritbox_fw.logging as logging
-from ghostsnstuff_spiritbox_fw.speech import STTClient, TTSClient
+from ghostsnstuff_spiritbox_fw.speech import STTClient, TTSClient, VOICE_MODELS
 import json
 import time
+import random
+import numpy as np
 
-from ghostsnstuff_spiritbox_fw.hal.emf import EMFDriver
-from ghostsnstuff_spiritbox_fw.hal.display import getDisplay
-from ghostsnstuff_spiritbox_fw.hal.speaker import getSpeaker
-from ghostsnstuff_spiritbox_fw.hal.microphone import getMic
+from ghostsnstuff_spiritbox_fw.hal.emf import get_emf_driver
+from ghostsnstuff_spiritbox_fw.hal.display import get_display
+from ghostsnstuff_spiritbox_fw.hal.speaker import get_audio
+from ghostsnstuff_spiritbox_fw.hal.microphone import get_microphone
 
 load_dotenv()
 
@@ -45,14 +47,19 @@ stt = STTClient(client)
 tts = TTSClient(client)
 
 ###### Initialize HAL
-emf = EMFDriver()
-disp = getDisplay()
-spk = getSpeaker()
-mic = getMic()
+emf = get_emf_driver()
+disp = get_display()
+spk = get_audio()
+mic = get_microphone()
 
 disp.begin()
-spk.begin()
-mic.registerIconCallback(disp.micIcon)
+disp.enable_sweep(True)
+spk.set_interference_volume(0.5)
+spk.set_interference_level(1)
+
+def handle_mic_icon(enabled):
+    disp.set_icon_state(mic=enabled)
+mic.register_icon_callback(handle_mic_icon)
 
 logging.print("Init OK")
 logging.print("Starting scenario")
@@ -62,6 +69,7 @@ logging.print("Starting scenario")
 PRIMARY_GHOST_VOICE = "onyx"
 SECONDARY_GHOST_VOICE = "nova"
 VOICE_SPEED = 0.75
+TTS_SAMPLE_RATE = 16000
 
 def polish_to_english(text):
     polishenglish = str.maketrans(
@@ -70,115 +78,119 @@ def polish_to_english(text):
     )
     return text.translate(polishenglish)
 
+def execute_glitch():
+    disp.enable_glitch(True)
+    spk.set_interference_level(2)
+    emf.glitch()
+    time.sleep(0.5)
+    spk.set_interference_level(1)
+    disp.enable_glitch(False)
 
-def get_buffer_length(buffer, sample_rate, num_channels):
-    num_samples = buffer.size
-    return num_samples / (sample_rate*num_channels)
+def play_ghost_speech(content: str, buffer: np.ndarray):
+    disp.set_icon_state(response=True)
 
-def handle_ghost_actions(actions, voice):
+    sound_length = spk.play_buffer(buffer, TTS_SAMPLE_RATE)
+    text = polish_to_english(content) if sound_length > 1.1 else "SYSTEM ERROR"
+    time.sleep(sound_length)
+    disp.set_text(text, duration=2)
+    disp.set_icon_state(response=False)
+
+    time.sleep(random.uniform(0.5, 1.5))
+
+def handle_ghost_actions(actions: GhostActions, voice: VOICE_MODELS):
     if actions.glitch:
-        disp.glitch(True)
-        spk.setInterference(2)
-        emf.glitch()
-        time.sleep(0.5)
-        disp.glitch(False)
-        pass
+        execute_glitch()
+
     if not actions.speech:
         return
+    
     if isinstance(actions.speech, list):
-        buffers = tts.synthesize_batch(actions.speech, voice, 0.75)
+        buffers = tts.synthesize_batch(actions.speech, voice, VOICE_SPEED)
         for word, buffer in zip(actions.speech, buffers):
-            print("PLAYING MULTIPLE")
-            disp.printText(polish_to_english(word))
-            spk.playBuffer(buffer, 16000)
-            disp.responseIcon(True)
-            #time.sleep(1.5)
-            buffer_lenght = get_buffer_length(buffer, 16000 ,1)
-            time.sleep(max(0, buffer_lenght))
-            if buffer_lenght < 2:
-                time.sleep(2-buffer_lenght)
-            disp.responseIcon(False)
+            word = word.strip()
+            if not word.endswith("..."):
+                word = f"{word}..."
+
+            play_ghost_speech(word, buffer)
     else:
-        buffer = tts.synthesize(actions.speech, voice, 0.75)
-        print("PLAYING ONE")
-        spk.playBuffer(buffer, 16000)
-        disp.responseIcon(True)
-        #time.sleep(1)
-        disp.printText("XXXXXX")
-        buffer_lenght = get_buffer_length(buffer, 16000 ,1)
-        time.sleep(max(0, buffer_lenght))
-        if buffer_lenght < 2:
-            time.sleep(2-buffer_lenght)
-        disp.responseIcon(False)
+        buffer = tts.synthesize(actions.speech, voice, VOICE_SPEED)
+        play_ghost_speech("XXXXXX", buffer)
 
 def win_condition():
-    spk.setInterference(3)
-    disp.glitch(True)
+    spk.set_interference_level(3)
+    disp.enable_glitch(True)
     emf.set_activity(6)
     time.sleep(5)
     for i in range(5):
         emf.set_activity(5 - i)
         time.sleep(1)
     time.sleep(2)
-   #emf.set_activity(0)
-    disp.glitch(False)
-    spk.setInterference(0)
-    disp.sweep(False)
+    emf.set_activity(0)
+    disp.enable_glitch(False)
+    spk.set_interference_level(0)
+    disp.enable_sweep(False)
     time.sleep(3)
-    disp.printText("Thank you")
+    disp.set_text("Thank you", duration=5)
     time.sleep(5)
-    disp.sweep(False)
+    disp.set_text(None)
     return
 
 def lose_condition():
     buffer = tts.synthesize("Głupcze...", "onyx", 0.5)
-    spk.setInterference(3)
-    disp.glitch(True)
+    spk.set_interference_level(3)
+    time.sleep(1)
     emf.set_activity(6)
+    time.sleep(0.5)
+    spk.play_buffer(buffer, TTS_SAMPLE_RATE)
     time.sleep(2)
-    disp.glitch(True)
-    for i in range(30):
+    disp.enable_glitch(True)
+    disp.enable_sweep(False)
+
+    for _ in range(30):
         emf.glitch()
         time.sleep(0.1)
 
-    time.sleep(2)
-    spk.playBuffer(buffer, 16000)
-    time.sleep(5)
-    disp.sweep(False)
+    time.sleep(4)
     emf.set_activity(0)
-    spk.setInterference(0)
-    disp.glitch(False)
+    spk.set_interference_level(0)
+    disp.enable_glitch(False)
+
+runtime.game_state.set_activity_level(3)
+
 while True:
-    disp.sweep(True)
-    buffer = mic.awaitBuffer()
-    disp.thinkingIcon(True)
-    user_query = stt.transcribe(buffer, 16000)
+    buffer = mic.await_buffer()
+    disp.set_icon_state(thinking=True)
+    user_query = stt.transcribe(buffer, mic.get_sample_rate())
     print(user_query)
     turn_result = runtime.execute(user_query)
-    disp.thinkingIcon(False)
+    disp.set_icon_state(thinking=False)
 
     if turn_result.game_result:
         if turn_result.game_result == "win":
             win_condition()
         else:
             lose_condition()
-        
         break
 
+    primary_actions = turn_result.primary_ghost_actions
+    primary_has_content = primary_actions and primary_actions.speech
+    secondary_actions = turn_result.secondary_ghost_actions
+    secondary_has_content = secondary_actions and secondary_actions.speech
+
     if turn_result.ghost_order == "primary":
-        if turn_result.primary_ghost_actions:
-            handle_ghost_actions(turn_result.primary_ghost_actions, PRIMARY_GHOST_VOICE)
-        if turn_result.secondary_ghost_actions:
-            handle_ghost_actions(turn_result.secondary_ghost_actions, SECONDARY_GHOST_VOICE)
+        if primary_actions:
+            handle_ghost_actions(primary_actions, PRIMARY_GHOST_VOICE)
+        if secondary_actions:
+            handle_ghost_actions(secondary_actions, SECONDARY_GHOST_VOICE)
     else:
-        if turn_result.secondary_ghost_actions:
-            handle_ghost_actions(turn_result.secondary_ghost_actions, SECONDARY_GHOST_VOICE)
-        if turn_result.primary_ghost_actions:
-            handle_ghost_actions(turn_result.primary_ghost_actions, PRIMARY_GHOST_VOICE)
+        if secondary_actions:
+            handle_ghost_actions(secondary_actions, SECONDARY_GHOST_VOICE)
+        if primary_actions:
+            handle_ghost_actions(primary_actions, PRIMARY_GHOST_VOICE)
     
-    if not turn_result.ghost_order == "primary" and turn_result.ghost_order == "secondary":
-        disp.noResponseIcon(True)
+    if not primary_has_content and not secondary_has_content:
+        disp.set_icon_state(no_response=True)
         time.sleep(2)
-        disp.noResponseIcon(False)
+        disp.set_icon_state(no_response=False)
 
     emf.set_activity(max(1, int(round(turn_result.activity_level / 2))))
